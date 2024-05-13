e729997da8f661287f6cc384808082fc1cd84c20
Luong Kien Minh <luongkienminh.it@gmail.com>
2024-05-13 22:07:15 +0700

    add simple contacts service

---
+++ src/mockData.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: "a1",
+    fullName: "John Doe",
+    email: "john.doe@gmail.com",
+    avatar: "",
+  },
+  {
+    id: "a2",
+    fullName: "Mary Jean",
+    email: "mary.jean@gmail.com",
+    avatar: "",
+  },
+  {
+    id: "a3",
+    fullName: "Susan Smith",
+    email: "susan.smith@gmail.com",
+    avatar: "",
+  },
+];
---
+++ src/server.js
@@ -0,0 +1,77 @@
+var PROTO_PATH = __dirname + "/../proto/index.proto";
+
+var grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+var contact_proto = grpc.loadPackageDefinition(packageDefinition).contactsproto;
+
+const contacts = require("./mockData");
+
+/**
+ * Implements the SayHello RPC method.
+ */
+function sayHello(call, callback) {
+  callback(null, { message: "Hello guys" + call.request.name });
+}
+
+function getContacts(call, callback) {
+  callback(null, { contacts });
+}
+
+function getContact(call, callback) {
+  const contactId = call.request.id;
+  const contact = contacts.find((c) => c.id === contactId);
+  callback(null, contact);
+}
+
+function deleteContact(call, callback) {
+  const contactId = call.request.id;
+  const index = contacts.findIndex((c) => c.id === contactId);
+  contacts.splice(index, 1);
+  callback(null, {});
+}
+
+function createContact(call, callback) {
+  const { fullName, email, avatar } = call.request;
+  const newContact = {
+    id: Date.now().toString(),
+    fullName,
+    email,
+    avatar,
+  };
+  contacts.push(newContact);
+  callback(null, newContact);
+}
+
+function main() {
+  var server = new grpc.Server();
+  server.addService(contact_proto.Contacts.service, {
+    sayHello: sayHello,
+    getContacts: getContacts,
+    getContact: getContact,
+    deleteContact,
+    createContact,
+  });
+
+  const port = 50051;
+
+  server.bindAsync(
+    `0.0.0.0:${port}`,
+    grpc.ServerCredentials.createInsecure(),
+    (error, runningPort) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(`Server started at port ${runningPort}`);
+      }
+    }
+  );
+}
+
+main();
