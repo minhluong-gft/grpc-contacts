10c04bca34fe744e9672b1c6bab5c99876a1b803
Luong Kien Minh <luongkienminh.it@gmail.com>
2024-05-13 22:29:39 +0700

    update mock data

--- src/mockData.js
+++ src/mockData.js
@@ -1,20 +1,132 @@
 module.exports = [
   {
-    id: "a1",
-    fullName: "John Doe",
-    email: "john.doe@gmail.com",
-    avatar: "",
+    id: "50db8600-737f-4b48-8e75-8d856b714a43",
+    fullName: "Dustin Dooley",
+    email: "Cale64@yahoo.com",
+    avatar: "https://avatars.githubusercontent.com/u/45549674",
   },
   {
-    id: "a2",
-    fullName: "Mary Jean",
-    email: "mary.jean@gmail.com",
-    avatar: "",
+    id: "e6ede85f-4c82-4e91-b1b8-3c1a24cc4e6e",
+    fullName: "Nina Schuppe",
+    email: "Warren17@hotmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/50544023",
   },
   {
-    id: "a3",
-    fullName: "Susan Smith",
-    email: "susan.smith@gmail.com",
-    avatar: "",
+    id: "2f92c6df-1a75-4d75-8904-ae3498751bbe",
+    fullName: "Blanche Moore",
+    email: "Boyd98@yahoo.com",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/332.jpg",
+  },
+  {
+    id: "b7b6d2e3-f2e4-413c-9778-92ce573e35f8",
+    fullName: "Jessica Brakus",
+    email: "Ted45@yahoo.com",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/332.jpg",
+  },
+  {
+    id: "094f6fa5-dba7-4a03-8520-8d266ccd064c",
+    fullName: "Roland Gottlieb",
+    email: "Bartholome62@hotmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/521233",
+  },
+  {
+    id: "c2940533-d007-4f5a-bee0-be56edddeee7",
+    fullName: "Aaron Franey DDS",
+    email: "Dario_Koelpin-Wiegand91@yahoo.com",
+    avatar: "https://avatars.githubusercontent.com/u/36167833",
+  },
+  {
+    id: "da7577b2-baf0-4670-9e5a-d82175a431ce",
+    fullName: "Ross Osinski",
+    email: "Dakota_Little81@yahoo.com",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1110.jpg",
+  },
+  {
+    id: "a69c4646-4819-49bd-b188-4bc840623357",
+    fullName: "Alex Langworth",
+    email: "Jerod_Hettinger@yahoo.com",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/968.jpg",
+  },
+  {
+    id: "bf3f888b-d924-4c98-a930-c6e1f2d6654d",
+    fullName: "Cynthia Stokes",
+    email: "Helena49@yahoo.com",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1054.jpg",
+  },
+  {
+    id: "969743e6-b3a7-4b3a-8405-098193f88e23",
+    fullName: "Dr. Nicholas Gusikowski",
+    email: "Angelo65@hotmail.com",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/165.jpg",
+  },
+  {
+    id: "3ea5c023-90f3-4404-a9e9-f1148d14ef0a",
+    fullName: "Jana Schulist III",
+    email: "Cecil30@hotmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/11584012",
+  },
+  {
+    id: "c26490bc-f404-4446-933c-28e63c356da1",
+    fullName: "Pat Haag",
+    email: "Dennis.Friesen@yahoo.com",
+    avatar: "https://avatars.githubusercontent.com/u/27772202",
+  },
+  {
+    id: "67df5ea8-2210-47d9-a7b3-7fcb759b8a2e",
+    fullName: "Denise Harber",
+    email: "Hoyt_Prohaska82@gmail.com",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1167.jpg",
+  },
+  {
+    id: "8aed1906-28aa-4c4f-bc0a-450607ea869e",
+    fullName: "Guy Parker",
+    email: "Kaleigh_Bartell16@yahoo.com",
+    avatar: "https://avatars.githubusercontent.com/u/26427468",
+  },
+  {
+    id: "40c3a582-d202-49c4-98cb-13decf015869",
+    fullName: "Celia Spencer",
+    email: "Laisha.Ortiz91@hotmail.com",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/764.jpg",
+  },
+  {
+    id: "91ee4b5e-c8c8-4e33-809f-da645b31960f",
+    fullName: "Sharon Ondricka",
+    email: "Ruthie55@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/13975557",
+  },
+  {
+    id: "16ef36b0-e411-4efa-867d-a61f4fc79d8e",
+    fullName: "Jennifer Skiles",
+    email: "Julia.Boehm@hotmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/20627446",
+  },
+  {
+    id: "fb80e9bb-0519-48c4-94df-299f8f183b2c",
+    fullName: "Jessie Cormier",
+    email: "Alexys.Stracke@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/81870510",
+  },
+  {
+    id: "92058cbc-8d4f-488a-b3f8-3485c948c877",
+    fullName: "Erma Thompson",
+    email: "Marilyne.Schoen@gmail.com",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/784.jpg",
+  },
+  {
+    id: "6ead23c1-300b-4d2f-b611-eff8d766204d",
+    fullName: "Emma Hudson",
+    email: "Dusty74@hotmail.com",
+    avatar:
+      "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/757.jpg",
   },
 ];
